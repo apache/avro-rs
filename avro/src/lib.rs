40def76b841d9172e48fbc1b0898ace5ba13f89f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-09-21 00:01:02 +0300

    Derive PartialEq to ExampleByteArray
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- avro/src/lib.rs
+++ avro/src/lib.rs
@@ -884,7 +884,7 @@
 //! ```rust
 //! use serde::{Deserialize, Serialize};
 //!
-//! #[derive(Debug, Deserialize, Serialize)]
+//! #[derive(Debug, Deserialize, PartialEq, Serialize)]
 //! struct ExampleByteArray {
 //!     #[serde(with = "apache_avro::serde_avro_bytes_opt")]
 //!     data_bytes: Option<Vec<u8>>,
