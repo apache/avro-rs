fe132e54ec7bab3fb196b483863a2c856e42346d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-01-05 14:57:09 +0200

    Fixes #385: Check that all source files have ASFv2 licence

--- avro/src/duration.rs
+++ avro/src/duration.rs
@@ -14,6 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
 /// A struct representing duration that hides the details of endianness and conversion between
 /// platform-native u32 and byte arrays.
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
