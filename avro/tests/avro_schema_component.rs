a1ec0ddfe40f939bc0a516d1f859228c7b47a582
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-01-12 16:20:48 +0200

    Add an IT test for AvroSchemaComponent

---
+++ avro/tests/avro_schema_component.rs
@@ -0,0 +1,42 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#[test]
+fn avro_rs_394_avro_schema_component_without_derive_feature() {
+    use apache_avro::{AvroSchemaComponent, Schema};
+    use std::collections::HashMap;
+
+    // Verify basic types work without derive feature
+    let schema = i32::get_schema_in_ctxt(&mut HashMap::default(), &None);
+    assert!(matches!(schema, Schema::Int));
+}
+
+#[test]
+fn avro_rs_394_avro_schema_component_nested_options() {
+    use apache_avro::{AvroSchemaComponent, Schema};
+    use std::collections::HashMap;
+
+    type VeryOptional = Option<Option<Option<Option<Option<i32>>>>>;
+
+    let schema = VeryOptional::get_schema_in_ctxt(&mut HashMap::default(), &None);
+    match schema {
+        Schema::Union(union_schema) => {
+            assert_eq!(union_schema.variants(), &[Schema::Null, Schema::Int]);
+        }
+        _ => panic!("Unexpected schema type: {schema}"),
+    };
+}
