4488a745ee4137ec0a0c8a98e5065717ad5291bd
default <admin@kriskras99.nl>
2026-03-03 15:51:29 +0000

    chore: Split `writer` module into several submodules

--- avro/src/lib.rs
+++ avro/src/lib.rs
@@ -99,8 +99,9 @@ pub use schema::Schema;
 pub use serde::{AvroSchema, AvroSchemaComponent, from_value, to_value};
 pub use uuid::Uuid;
 pub use writer::{
-    Clearable, GenericSingleObjectWriter, SpecificSingleObjectWriter, Writer, WriterBuilder,
-    to_avro_datum, to_avro_datum_schemata, write_avro_datum_ref,
+    Clearable, Writer, WriterBuilder,
+    datum::{to_avro_datum, to_avro_datum_schemata, write_avro_datum_ref},
+    single_object::{GenericSingleObjectWriter, SpecificSingleObjectWriter},
 };
 
 #[cfg(feature = "derive")]
