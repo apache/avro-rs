1166b36cb8c49b59e1d9763c4f4adc8c4008048e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-03-24 23:01:29 +0200

    Simplify the docstrings for logging related helper methods
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- avro_test_helper/src/logger.rs
+++ avro_test_helper/src/logger.rs
@@ -53,47 +53,33 @@ fn test_logger() -> &'static TestLogger {
     })
 }
 
-/// Clears all log messages stored in the thread-local storage.
-///
-/// # Panics
-/// This function will panic if:
-/// - The thread-local `LOG_MESSAGES` is already borrowed and cannot
-///   be mutably borrowed.
-///
-/// The panic includes a debug representation of the error encountered.
+/// Clears all log messages of this thread.
 pub fn clear_log_messages() {
     LOG_MESSAGES.with_borrow_mut(Vec::clear);
 }
 
-/// Asserts that a specific log message has not been logged.
-///
-/// This function checks the most recently logged message from a thread-local
-/// storage and ensures that it does not match the provided `unexpected_message`.
-/// If the message does match, the function will panic with an appropriate error
-/// message indicating the unexpected log entry.
-///
-/// # Arguments
-/// - `unexpected_message`: A string slice that represents the log message
-///   that is not expected to be logged. If this message matches the last
-///   logged message, the function will panic.
+/// Asserts that the message is not the last in the log for this thread.
 ///
 /// # Panics
-/// - This function will panic if the `unexpected_message` matches the most
-///   recently logged message.
+/// Will panic if the provided message is an exact match for the last log message.
 ///
 /// # Example
-/// ```ignore
-/// // Assume LOG_MESSAGES is set up to capture log messages.
-/// log_message("Unexpected Error");
-/// assert_not_logged("Unexpected Error");
-/// // This will panic with the message:
-/// // "The following log message should not have been logged: 'Unexpected Error'"
+/// ```should_panic
+/// log::error("Something went wrong");
+/// log::error("Unexpected Error");
 ///
-/// assert_not_logged("Non-existent Message");
-/// // This will pass without issue since the message was not logged.
+/// // This will not panic as it is not an exact match
+/// assert_not_logged("No Unexpected Error");
+///
+/// // This will not panic as it is not the last log message
+/// assert_not_logged("Something went wrong");
+///
+/// // This will panic
+/// assert_not_logged("Unexpected Error");
+/// ```
 #[track_caller]
 pub fn assert_not_logged(unexpected_message: &str) {
-    LOG_MESSAGES.with(|msgs| match msgs.borrow().last() {
+    LOG_MESSAGES.with_borrow(|msgs| match msgs.last() {
         Some(last_log) if last_log == unexpected_message => {
             panic!("The following log message should not have been logged: '{unexpected_message}'")
         }
@@ -101,43 +87,26 @@ pub fn assert_not_logged(unexpected_message: &str) {
     });
 }
 
-/// Asserts that the specified log message has been logged and removes it from
-/// the stored log messages.
-///
-/// # Parameters
-/// - `expected_message`: A string slice that holds the log message to be asserted.
+/// Asserts that the message has been logged and removes it from the log of this thread.
 ///
 /// # Panics
-/// This function will panic if the `expected_message` has not been logged. The
-/// panic message will indicate the missing log message.
-///
-/// # Behavior
-/// - The function verifies if the `expected_message` exists in the log messages
-///   stored using the thread-local storage (`LOG_MESSAGES`).
-/// - If the message is found, it is removed from the log messages and the function
-///   completes successfully.
-/// - If the message is not found, a panic is triggered with an explanatory message.
-///
-/// # Usage
-/// This function is typically used in unit tests or scenarios where it is important
-/// to ensure specific messages are logged during execution.
+/// Will panic if the message does not appear in the log.
 ///
 /// # Example
-/// ```ignore
-/// // Assuming LOG_MESSAGES is set up and some log messages have been recorded:
-/// assert_logged("Expected log entry");
+/// ```should_panic
+/// log::error("Something went wrong");
+/// log::info("Something happened");
+/// log::error("Something went wrong");
+///
+/// // This will not panic as the message was logged
+/// assert_logged("Something went wrong");
+///
+/// // This will not panic as the message was logged
+/// assert_logged("Something happened");
+///
+/// // This will panic as the first call removed the matching log messages
+/// assert_logged("Something went wrong");
 /// ```
-///
-/// # Notes
-/// - The function uses the `#[track_caller]` attribute to improve debugging by
-///   providing more accurate information about the location of the panic in the
-///   source code.
-/// - The `LOG_MESSAGES` must be a thread-local data structure that supports
-///   borrowing and mutating of a collection of string messages.
-///
-/// # Thread Safety
-/// This function relies on thread-local variables to manage logs for each thread
-/// independently.
 #[track_caller]
 pub fn assert_logged(expected_message: &str) {
     let mut deleted = false;
